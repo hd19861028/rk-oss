d89114a6397b892818746b39e8d8b240b5cd38a4
hujindi1 <hujindi@rayku.net>
2019-07-16 18:39:43 +0800

    1.0.6: oss-dir显示下载进度

--- cli/readdir.js
+++ cli/readdir.js
@@ -17,12 +17,13 @@ var argMap = args(process.argv.slice(2));
     }
 
     let result = await client.list({ prefix, delimiter: '/' });
+    var total = result.objects.length;
     for (var i = 0; i < result.objects.length; i++) {
         var file = result.objects[i];
         var remoteFile = file.name;
         var basename = path.basename(remoteFile);
         var localFile = path.resolve(output, basename)
         var r = await oss.get(client, remoteFile, localFile);
-        console.log(`${r}: ${localFile}`)
+        console.log(`${i+1}/${total}: ${localFile}`)
     }
 })()
