6a3b61ffac43637e59d22022a580be75c4f17971
hujindi1 <hujindi@rayku.net>
2019-07-24 18:03:51 +0800

    上传文件返回状态码

--- cli/putfile.js
+++ cli/putfile.js
@@ -13,5 +13,12 @@ var argMap = args(process.argv.slice(2));
     var client = oss.instance({ region, accessKeyId, accessKeySecret, bucket });
 
     var r = await oss.put(client, local, remote);
+    if (r) {
+        console.log(0);
+        process.exit(0);
+    } else {
+        console.log(1);
+        process.exit(1);
+    }
     console.log(`${r}: ${remote}`)
 })()
